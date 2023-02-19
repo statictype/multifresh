a76002560ad943480aecccd6c2edc6e2b060c408
Andreea Eftene <hi@pixelle.ro>
2023-02-20 00:15:02 +0100

    basic layout

--- components/PageWithContext.tsx
+++ components/PageWithContext.tsx
@@ -17,10 +17,16 @@ export function PageWithContext({ children, ...props }: Props) {
         <link rel="stylesheet" href="global.css" />
       </Head>
       <AppContextProvider {...props.data}>
-        <div className="grid gap-4 h-full">
-          <TopBar />
-          <NavBar />
-          {children}
+        <div className="grid grid-cols-12 grid-rows-12 gap-4 min-h-screen">
+          <div className="row-span-1 col-span-12">
+            <TopBar />
+          </div>
+          <div className="row-span-1 col-span-12 bg-blue-100">
+            <NavBar />
+          </div>
+          <div className="row-span-5 col-span-10 col-start-2 bg-blue-100">
+            {children}
+          </div>
         </div>
       </AppContextProvider>
     </>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,13 +1,24 @@
 import type { Handlers, PageProps } from "$fresh/server.ts";
 import { AppState } from "../types/index.ts";
 import { PageWithContext } from "../components/PageWithContext.tsx";
+import { useAppContext } from "../components/AppContext.tsx";
 
 export const handler: Handlers = {
   async GET(req, ctx) {
     return await ctx.render!(ctx.state);
   },
 };
+function Consumer() {
+  const { web3Account } = useAppContext();
 
+  return <span>Connected Account: {web3Account}</span>;
+}
 export default function Home(props: PageProps<AppState>) {
-  return <PageWithContext {...props}></PageWithContext>;
+  return (
+    <PageWithContext {...props}>
+      <div className="grid place-content-center bg-white h-full">
+        <Consumer />
+      </div>
+    </PageWithContext>
+  );
 }
