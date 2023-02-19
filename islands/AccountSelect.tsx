eb01f6cbffbecfc324f8e581707abd5c459fe1d0
Andreea Eftene <hi@pixelle.ro>
2023-02-19 13:06:11 +0100

    optimistic account select

--- islands/AccountSelect.tsx
+++ islands/AccountSelect.tsx
@@ -10,13 +10,15 @@ export default function AccountSelect(
     <>
       <form method="post" action="/api/wallet/setAccount" className="mr-6">
         <select name="selectedAccount" className="mr-3 w-24">
-          {accounts.value.map(({ address }) => {
-            return (
-              <option selected={address === web3Account}>
-                {address}
-              </option>
-            );
-          })}
+          {accounts.value.length > 0
+            ? accounts.value.map(({ address }) => {
+              return (
+                <option selected={address === web3Account}>
+                  {address}
+                </option>
+              );
+            })
+            : <option>{web3Account}</option>}
         </select>
         <button type="submit" className="px-4 px-2 bg-white">switch</button>
       </form>
